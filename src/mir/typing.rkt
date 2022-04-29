c6f3e11c90a7c629336db7a72db4b0b69d0d5a8e
Dominik Stolz <d.stolz@tum.de>
2022-04-29 11:38:21 +0200

    Add missing rules

--- src/mir/typing.rkt
+++ src/mir/typing.rkt
@@ -16,7 +16,7 @@
 
   [(where (_ ... (local-variable-id Ty_var) _ ...) Γ-ty)
    (where PlaceTy (apply-projections CrateDecls (TyPlace Ty_var) projections))
-   ---------------------------------------------------------------------------
+   --------------------------------------------------------------------------- "local"
    (types/place (Γ-ty CrateDecls) (local-variable-id projections) PlaceTy)]
 )
 
@@ -26,14 +26,14 @@
   #:contract (types/operand Γ operand Ty)
 
   [(types/place Γ place (TyPlace Ty))
-   -----------------------------------------
+   ----------------------------------------- "copy"
    (types/operand Γ (operand-copy place) Ty)]
 
   [(types/place Γ place (TyPlace Ty))
-   -----------------------------------------
+   ----------------------------------------- "move"
    (types/operand Γ (operand-move place) Ty)]
 
-  [------------------------------------------------------
+  [------------------------------------------------------ "const"
    (types/operand Γ (operand-constant _) (scalar-ty i32))]
 )
 
@@ -42,21 +42,84 @@
   #:mode (types/rvalue I I O)
   #:contract (types/rvalue Γ rvalue Ty)
 
-  [--------------------------------------------------
+  [-------------------------------------------------- "nullop"
    (types/rvalue Γ (nullary-op Ty) (scalar-ty usize))]
 
   [(types/operand Γ operand Ty)
-   ----------------------------------------
+   ---------------------------------------- "use"
    (types/rvalue Γ (rvalue-use operand) Ty)]
 
   [(types/place Γ place (TyPlace Ty))
-   --------------------------------------------------------------------------------
+   -------------------------------------------------------------------------------- "ref"
    (types/rvalue Γ (rvalue-ref Lt MaybeMut place) (TyRigid (Ref MaybeMut) (Lt Ty)))]
 
   [(types/operand Γ operand_a (TyRigid ScalarId_ty ()))
    (types/operand Γ operand_b (TyRigid ScalarId_ty ()))
-   ----------------------------------------------------------------------------------
+   ---------------------------------------------------------------------------------- "binop"
    (types/rvalue Γ (rvalue-binary-op _ operand_a operand_b) (TyRigid ScalarId_ty ()))]
+)
+
+(define-judgment-form
+  formality-mir+Γ
+  #:mode (types/stmt I I O)
+  #:contract (types/stmt Γ statement Γ)
+
+  [-------------------------------- "nop"
+   (types/stmt Γ (statement-nop) Γ)]
+
+  [----------------------------------------- "storage-live"
+   (types/stmt Γ (statement-storage-live) Γ)]
+
+  [----------------------------------------- "storage-dead"
+   (types/stmt Γ (statement-storage-dead) Γ)]
+
+  [(types/rvalue Γ rvalue Ty)
+   (types/place Γ place (TyPlace Ty))
+   ------------------------------------------------ "assign"
+   (types/stmt Γ (statement-assign place rvalue) Γ)]
+
+  [(types/place Γ place (TyPlace (TyRigid AdtId _)))
+   (where/error (_ CrateDecls) Γ)
+   (where (enum _ _ AdtVariants) (item-with-id CrateDecls AdtId))
+   (where (_ ... (VariantId _) _ ...) AdtVariants)
+   -------------------------------------------------------------- "set-discr"
+   (types/stmt Γ (statement-set-discriminant place VariantId) Γ)]
+)
+
+(define-judgment-form
+  formality-mir+Γ
+  #:mode (types/term I I O)
+  #:contract (types/term Γ terminator Γ)
+
+  [------------------------------------ "goto"
+   (types/term Γ (terminator-goto _) Γ)]
+
+  [---------------------------------- "resume"
+   (types/term Γ terminator-resume Γ)]
+
+  [--------------------------------- "abort"
+   (types/term Γ terminator-abort Γ)]
+
+  [---------------------------------- "return"
+   (types/term Γ terminator-return Γ)]
+
+  [--------------------------------------- "unreachable"
+   (types/term Γ terminator-unreachable Γ)]
+
+  [(types/place Γ place _)
+   ------------------------------------------ "drop"
+   (types/term Γ (terminator-drop place _) Γ)]
+
+  [(types/place Γ place (TyPlace Ty))
+   (types/operand Γ operand Ty)
+   -------------------------------------------------------------- "drop-replace"
+   (types/term Γ (terminator-drop-and-replace place operand _) Γ)]
+
+  [(types/operand Γ operand_fn (TyRigid (Fn _ _) (Ty_arg ..._n Ty_ret)))
+   (types/operand Γ operand_arg Ty_arg) ...
+   (types/place Γ place (TyPlace Ty_ret))
+   ------------------------------------------------------------------------- "call"
+   (types/term Γ (terminator-call operand_fn (operand_arg ..._n) place _) Γ)]
 )
 
 (define-judgment-form
@@ -65,39 +128,13 @@
   #:contract (types/block Γ basic-block-data Γ)
 
   [(types/term Γ_0 terminator Γ_1)
-   -------------------------------------
+   ------------------------------------- "block-base"
    (types/block Γ_0 (() terminator) Γ_1)]
 
   [(types/stmt Γ_0 statement_hd Γ_1)
    (types/block Γ_1 ((statement_tl ...) terminator) Γ_2)
-   ------------------------------------------------------------------
+   ------------------------------------------------------------------ "block-step"
    (types/block Γ_0 ((statement_hd statement_tl ...) terminator) Γ_2)]
-)
-
-(define-judgment-form
-  formality-mir+Γ
-  #:mode (types/stmt I I O)
-  #:contract (types/stmt Γ statement Γ)
-
-  [--------------------------------
-   (types/stmt Γ (statement-nop) Γ)]
-
-  [(types/rvalue Γ rvalue Ty)
-   (types/place Γ place (TyPlace Ty))
-   ------------------------------------------------
-   (types/stmt Γ (statement-assign place rvalue) Γ)]
-)
-
-(define-judgment-form
-  formality-mir+Γ
-  #:mode (types/term I I O)
-  #:contract (types/term Γ terminator Γ)
-
-  [(types/operand Γ operand_fn (TyRigid (Fn _ _) (Ty_arg ..._n Ty_ret)))
-   (types/operand Γ operand_arg Ty_arg) ...
-   (types/place Γ place (TyPlace Ty_ret))
-   -------------------------------------------------------------------------
-   (types/term Γ (terminator-call operand_fn (operand_arg ..._n) place _) Γ)]
 )
 
 (define-metafunction formality-mir+Γ
