4c478d71ffba7e14fa8fbef41c49f52b9bc177a4
Mario Carneiro <di.gama@gmail.com>
2022-05-17 15:37:06 -0400

    fix

--- src/ty/test/test-outlives.rkt
+++ src/ty/test/test-outlives.rkt
@@ -74,7 +74,7 @@
                    Env
                    ()
                    ()
-                   ((∀ ((lifetime A)) (rigid-ty (fn-ptr "" 1) ((rigid-ty (ref ()) (A (scalar-ty u32))) TyUnit)))
+                   ((∀ ((lifetime A)) (rigid-ty (fn-ptr "" 1) ((rigid-ty (ref ()) (A (scalar-ty u32))) unit-ty)))
                     -outlives-
                     static)
                    ))
