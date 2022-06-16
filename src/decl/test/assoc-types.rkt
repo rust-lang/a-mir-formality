87af4e3cab121af5585ce8394f281b8c15a21e64
Niko Matsakis <niko@alum.mit.edu>
2022-06-16 05:08:07 -0400

    exhaustive tests

--- src/decl/test/assoc-types.rkt
+++ src/decl/test/assoc-types.rkt
@@ -10,81 +10,92 @@
 (module+ test
   (current-traced-metafunctions '(relate/one compare/one/substituted equate/one/substituted))
 
+  (define-metafunction formality-decl
+    trait-decl-with-bound : TraitId BoundsClause -> TraitDecl
 
-  (;; positive test: trait requests Sized, and impl supplies it
-   redex-let*
+    [(trait-decl-with-bound TraitId BoundsClause)
+     (trait TraitId ((type Self)) where ()
+            {
+             (type Item () BoundsClause where ())
+             })
+     ]
+    )
+
+  (redex-let*
    formality-decl
 
    [(; trait Iterator { type Item: Sized; }
-     TraitDecl_Iterator (term (trait Iterator ((type Self)) where ()
-                                     {
-                                      (type Item () (: (type Item) ((Item : rust:Sized ()))) where ())
-                                      })))
+     TraitDecl_Iterator_where_Item:Sized
+     (term (trait-decl-with-bound Iterator (: (type Item) ((Item : rust:Sized ())))))
+     )
+
+    (; trait Iterator { type Item: ?Sized; }
+     TraitDecl_Iterator_where_Item:?Sized
+     (term (trait-decl-with-bound Iterator (: (type Item) ())))
+     )
 
     (; struct MyIter<T: ?Sized> { }
      AdtDecl_MyIter (term (struct MyIter ((type T)) where () ((MyIter ())))))
 
     (; impl<T: Sized> Iterator for MyIter<T> { type Item = T; }
-     TraitImplDecl_MyIter_Iterator (term (impl ((type T)) (Iterator ((user-ty (MyIter T))))
-                                               where ((T : rust:Sized ()))
-                                               {
-                                                (type Item () = T where ())
-                                                })))
+     TraitImplDecl_Iterator_for_MyIter<T>_where_T:Sized
+     (term (impl ((type T)) (Iterator ((user-ty (MyIter T))))
+                 where ((T : rust:Sized ()))
+                 {
+                  (type Item () = T where ())
+                  })))
 
+    (; impl<T: Sized> Iterator for MyIter<T> { type Item = T; }
+     TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Sized
+     (term (impl ((type T)) (Iterator ((user-ty (MyIter T))))
+                 where ()
+                 {
+                  (type Item () = T where ())
+                  })))
     ]
 
-   (; test that the above crate is WF
-    redex-let*
-    formality-decl
-    [
-     (CrateDecl (term (C (crate (TraitDecl_Iterator
-                                 AdtDecl_MyIter
-                                 TraitImplDecl_MyIter_Iterator
-                                 )))))
-     ]
+   (; Trait requires Sized, impl supplies Sized: OK
+    traced '()
+           (decl:test-crate-decl-ok [core-crate-decl
+                                     (C (crate [AdtDecl_MyIter
+                                                TraitDecl_Iterator_where_Item:Sized
+                                                TraitImplDecl_Iterator_for_MyIter<T>_where_T:Sized
+                                                ]))
+                                     ]
+                                    C)
+           )
 
-    (traced '()
-            (decl:test-crate-decl-ok (CrateDecl core-crate-decl) C)
-            )
-    )
+   (; Trait requires ?Sized, impl supplies Sized: OK
+    traced '()
+           (decl:test-crate-decl-ok [core-crate-decl
+                                     (C (crate [AdtDecl_MyIter
+                                                TraitDecl_Iterator_where_Item:?Sized
+                                                TraitImplDecl_Iterator_for_MyIter<T>_where_T:Sized
+                                                ]))
+                                     ]
+                                    C)
+           )
+
+   (; Trait requires Sized, impl supplies ?Sized: not OK
+    traced '()
+           (decl:test-crate-decl-not-ok [core-crate-decl
+                                         (C (crate [AdtDecl_MyIter
+                                                    TraitDecl_Iterator_where_Item:Sized
+                                                    TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Sized
+                                                    ]))
+                                         ]
+                                        C)
+           )
+
+   (; Trait requires ?Sized, impl supplies ?Sized: OK
+    traced '()
+           (decl:test-crate-decl-ok [core-crate-decl
+                                     (C (crate [AdtDecl_MyIter
+                                                TraitDecl_Iterator_where_Item:?Sized
+                                                TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Sized
+                                                ]))
+                                     ]
+                                    C)
+           )
    )
-
-  (;; negative test: the impl doesn't supply Sized
-   redex-let*
-   formality-decl
-
-   [(; trait Iterator { type Item: Sized; }
-     TraitDecl_Iterator (term (trait Iterator ((type Self)) where ()
-                                     {
-                                      (type Item () (: (type Item) ((Item : rust:Sized ()))) where ())
-                                      })))
-
-    (; struct MyIter<T: ?Sized> { }
-     AdtDecl_MyIter (term (struct MyIter ((type T)) where () ((MyIter ())))))
-
-    (; impl<T: ?Sized> Iterator for MyIter<T> { type Item = T; }
-     TraitImplDecl_MyIter_Iterator (term (impl ((type T)) (Iterator ((user-ty (MyIter T))))
-                                               where () ; missing where clause here
-                                               {
-                                                (type Item () = T where ())
-                                                })))
-
-    ]
-
-   (; above crate is not valid
-    redex-let*
-    formality-decl
-    [
-     (CrateDecl (term (C (crate (TraitDecl_Iterator
-                                 AdtDecl_MyIter
-                                 TraitImplDecl_MyIter_Iterator
-                                 )))))
-     ]
-
-    (traced '()
-            (decl:test-crate-decl-not-ok (CrateDecl core-crate-decl) C)
-            )
-    )
-   )
-
   )
