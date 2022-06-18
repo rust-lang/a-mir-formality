81991c578b2cadea33c392f62f5189d3e80f4f24
Niko Matsakis <niko@alum.mit.edu>
2022-06-18 06:44:53 -0400

    improve the test
    shows that we can prove things that require normalizing

--- src/decl/test/assoc-type-values-normalize.rkt
+++ src/decl/test/assoc-type-values-normalize.rkt
@@ -10,27 +10,19 @@
 (module+ test
   (current-traced-metafunctions '(relate/one compare/one/substituted equate/one/substituted))
 
-  (define-metafunction formality-decl
-    trait-decl-with-bound : TraitId BoundsClause -> TraitDecl
-
-    [(trait-decl-with-bound TraitId BoundsClause)
-     (trait TraitId ((type Self)) where ()
-            {
-             (type Item () BoundsClause where ())
-             })
-     ]
-    )
-
   (redex-let*
    formality-decl
 
-   [(; trait Iterator { type Item: ?Sized; }
-     TraitDecl_Iterator_where_Item:?Sized
-     (term (trait-decl-with-bound Iterator (: (type Item) ())))
+   [(; trait Iterator { type Item; }
+     TraitDecl_Iterator_where_Item
+     (term (trait Iterator[(type Self)] where ()
+                  {
+                   (type Item () (: (type Self) []) where ())
+                   }))
      )
 
-    (; impl<T: ?Sized> Iterator for (T,) { type Item = Once; }
-     TraitImplDecl_Iterator_for_Once<T>_where_T:?Sized
+    (; impl<T> Iterator for (T,) { type Item = Once; }
+     TraitImplDecl_Iterator_for_Once<T>
      (term (impl ((type T)) (Iterator ((user-ty (tuple T))))
                  where ()
                  {
@@ -38,46 +30,53 @@
                   })))
 
     (; struct Once<T> { }
-     AdtDecl_Once<T:Sized>
+     AdtDecl_Once<T>
      (term (struct Once ((type T))
-             where ((T : rust:Sized()))
+             where ()
              ((Once ())))))
 
+    (; impl<T> Copy for Once<T> where T: Copy { }
+     TraitImplDecl_Copy_for_Once<T>
+     (term (impl[(type T)] (rust:Copy[(user-ty (Once T))])
+                where [(T : rust:Copy[])]
+                {})))
+
     (CrateDecl_C
-     (term (C (crate [TraitDecl_Iterator_where_Item:?Sized
-                      TraitImplDecl_Iterator_for_Once<T>_where_T:?Sized
-                      AdtDecl_Once<T:Sized>
+     (term (C (crate [TraitDecl_Iterator_where_Item
+                      TraitImplDecl_Iterator_for_Once<T>
+                      AdtDecl_Once<T>
+                      TraitImplDecl_Copy_for_Once<T>
                       ]))))
 
-    (CrateDecls (term [core-crate-decl
-                       (C (crate [TraitDecl_Iterator_where_Item:?Sized
-                                  TraitImplDecl_Iterator_for_Once<T>_where_T:?Sized
-                                  AdtDecl_Once<T:Sized>
-                                  ]))
-                       ]))
+    (CrateDecls
+     (term [core-crate-decl
+            CrateDecl_C
+            ]))
 
     ]
 
-   (redex-let*
-    formality-decl
-    [(Goal_eq (term ((user-ty (@ (Iterator Item) (tuple u32)))
-                     ==
-                     (user-ty u32))))]
+   (; Cannot normalize `(T,)` to `T`
+    traced '()
+           (test-equal
+            (term (decl:can-prove-goal CrateDecls C ((user-ty (@ (Iterator Item) (tuple u32)))
+                                                     ==
+                                                     (user-ty u32))))
+            #f))
 
-    (; Cannot normalize `(T,)` to `T`
-     traced '()
-            (test-equal
-             (term (decl:can-prove-goal CrateDecls C Goal_eq))
-             #f))
+   (; Can normalize `(T,)` to `Once<T>`
+    traced '()
+           (test-equal
+            (term (decl:can-prove-goal CrateDecls C ((user-ty (@ (Iterator Item) (tuple u32)))
+                                                     ==
+                                                     (user-ty (Once u32)))))
+            #t))
 
-    (; Can normalize `(T,)` to `Once<T>`
-     traced '()
-            (test-equal
-             (term (decl:can-prove-goal CrateDecls C
-                                        ((user-ty (@ (Iterator Item) (tuple u32)))
-                                         ==
-                                         (user-ty (Once u32)))))
-             #t))
-    )
+   (; Test that `<(u32,) as Iterator>::Item` implements `Copy`
+    traced '()
+           (test-equal
+            (term (decl:can-prove-goal
+                   CrateDecls C
+                   (is-implemented (rust:Copy[(user-ty (@ (Iterator Item) (tuple u32)))]))))
+            #t))
    )
   )
