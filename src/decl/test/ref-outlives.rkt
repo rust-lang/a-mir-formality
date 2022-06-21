4ffb53325ba1387ff6baa78f54f9f53cfc29c11d
Niko Matsakis <niko@alum.mit.edu>
2022-06-21 06:21:21 -0400

    add tests for outlives and WF for refs

---
+++ src/decl/test/ref-outlives.rkt
@@ -0,0 +1,48 @@
+#lang racket
+(require redex/reduction-semantics
+         "../grammar.rkt"
+         "../prove.rkt"
+         "../../ty/user-ty.rkt"
+         "../../util.rkt"
+         "libcore.rkt"
+         )
+
+(module+ test
+
+  (; &'a T is WF if T: 'a...
+   traced '()
+          (test-equal
+           (term (decl:can-prove-goal
+                  [core-crate-decl]
+                  core
+                  (∀ [(type T) (lifetime a)]
+                     (implies [(T -outlives- a)]
+                              (well-formed (type (user-ty (& a T))))))))
+           #t
+           ))
+
+  (; ...and if `T: 'b` and `'b: 'a`...
+   traced '()
+          (test-equal
+           (term (decl:can-prove-goal
+                  [core-crate-decl]
+                  core
+                  (∀ [(type T) (lifetime a) (lifetime b)]
+                     (implies [(T -outlives- b) (b -outlives- a)]
+                              (well-formed (type (user-ty (& a T))))))))
+           #t
+           ))
+
+  (; ...but not if `T` and `'a` have no relationship.
+   traced '()
+          (test-equal
+           (term (decl:can-prove-goal
+                  [core-crate-decl]
+                  core
+                  (∀ [(type T) (lifetime a)]
+                     (implies []
+                              (well-formed (type (user-ty (& a T))))))))
+           #f
+           ))
+
+  )
