0d34189a95531eab14e12a636c7f22224e5412e2
Niko Matsakis <niko@alum.mit.edu>
2022-06-21 06:08:16 -0400

    rewrite the WF test to use Copy, not Sized
    Sized was kind of confusing, Copy is clearer

--- src/decl/test/assoc-type-values-wf.rkt
+++ src/decl/test/assoc-type-values-wf.rkt
@@ -24,52 +24,64 @@
   (redex-let*
    formality-decl
 
-   [(; trait Iterator { type Item: ?Sized; }
-     TraitDecl_Iterator_where_Item:?Sized
+   [(; trait Iterator { type Item; }
+     TraitDecl_Iterator_where_Item:?Copy
      (term (trait-decl-with-bound Iterator (: (type Item) ())))
      )
 
-    (; impl<T: ?Sized> Iterator for (T,) { type Item = MyIter<T>; }
-     TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Sized
+    (; impl<T> Iterator for (T,) { type Item = MyIter<T>; }
+     TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Copy
      (term (impl ((type T)) (Iterator ((user-ty (tuple T))))
                  where ()
                  {
                   (type Item () = (user-ty (MyIter T)) where ())
                   })))
 
-    (; struct MyIter<T> { }
-     AdtDecl_MyIter<T:Sized>
+    (; struct MyIter<T: Copy> { }
+     AdtDecl_MyIter<T:Copy>
      (term (struct MyIter ((type T))
-             where ((T : rust:Sized()))
+             where ((T : rust:Copy()))
              ((MyIter ())))))
 
-    (; struct MyIter<T: ?Sized> { }
-     AdtDecl_MyIter<T:?Sized>
+    (; struct MyIter<T: ?Copy> { }
+     AdtDecl_MyIter<T:?Copy>
      (term (struct MyIter ((type T))
              where ()
              ((MyIter ())))))
     ]
 
-   (; Impl supplies ?Sized, MyIter requires ?Sized: OK
-    traced '()
-           (decl:test-crate-decl-ok [core-crate-decl
-                                     (C (crate [TraitDecl_Iterator_where_Item:?Sized
-                                                TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Sized
-                                                AdtDecl_MyIter<T:?Sized>
+   #;(; Impl supplies ?Copy, MyIter requires ?Copy: OK XXX
+      traced '()
+             (test-equal
+              (term (decl:is-crate-ok [core-crate-decl
+                                       (C (crate [TraitDecl_Iterator_where_Item:?Copy
+                                                  TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Copy
+                                                  AdtDecl_MyIter<T:?Copy>
+                                                  ]))
+                                       ]
+                                      C))
+              #t)
+             )
+
+   (; Impl supplies ?Copy, MyIter requires Sized: not OK
+    ;
+    ; FIXME -- this currently succeeds because
+    ;
+    ; - we can show that (@ (Iterator Item) (T,)) is WF (that requires the impl to apply, and it does)
+    ; - we can show that it normalizes to Adt<T>
+    ;
+    ; In other words, it's a kind of "self-supporting impl" problem! If we are able to show that
+    ; the type X is WF because we can find something that normalizes to it, that's a problem.
+    traced '(prove program-rules)
+           (test-equal
+            (term (decl:is-crate-ok [core-crate-decl
+                                     (C (crate [TraitDecl_Iterator_where_Item:?Copy
+                                                TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Copy
+                                                AdtDecl_MyIter<T:Copy>
                                                 ]))
                                      ]
-                                    C)
-           )
-
-   (; Impl supplies ?Sized, MyIter requires Sized: not OK
-    traced '()
-           (decl:test-crate-decl-not-ok [core-crate-decl
-                                         (C (crate [TraitDecl_Iterator_where_Item:?Sized
-                                                    TraitImplDecl_Iterator_for_MyIter<T>_where_T:?Sized
-                                                    AdtDecl_MyIter<T:Sized>
-                                                    ]))
-                                         ]
-                                        C)
-           )
+                                    C))
+            #f
+            ))
    )
   )
