071e33cec05c6a57531c64f25a81bdd5da20a5b7
Niko Matsakis <niko@alum.mit.edu>
2022-07-09 07:26:11 -0400

    add more tests for GATs behaving as expected
    e.g., given `for<'a> T::Item<'a>` doesn't permit
    `T::Item<'static>` (unless `T: 'static`).

--- src/rust/test/wf--hrtb-gats.rkt
+++ src/rust/test/wf--hrtb-gats.rkt
@@ -10,18 +10,20 @@
   (redex-let*
    formality-rust
 
-   [(Rust/TraitDecl_gat (term (trait Ref[]
-                                     where []
-                                     { (type Item[(lifetime a)] : [] where [(Self : a)])
-                                       })))
+   [(Rust/Program (term ([(crate C { (trait Ref[]
+                                            where []
+                                            { (type Item[(lifetime a)] : [] where [(Self : a)])
+                                              })
 
-    (Rust/TraitImplDecl_gat (term (impl[(type T)] Ref[] for T
-                                       where []
-                                       { (type Item[(lifetime a)] = (& a T) where [(T : a)])
-                                         })))
+                                     (impl[(type T)] Ref[] for T
+                                          where []
+                                          { (type Item[(lifetime a)] = (& a T) where [(T : a)])
+                                            })
 
-    (Rust/Program (term ([(crate C { Rust/TraitDecl_gat
-                                     Rust/TraitImplDecl_gat
+                                     (trait OK[] where [] {})
+
+                                     (impl[(type T)] OK[] for T where [] {})
+
                                      })] C)))
 
     ]
@@ -40,5 +42,52 @@
                 )
              )
             ))
+
+   (traced '()
+           (test-term-true
+            (rust:can-prove-where-clause-in-program
+             Rust/Program
+             (∀ [(type T)]
+                where []
+                ; key point here:
+                ;
+                ;     same as above but with a generic type T
+                (for[(lifetime l)] (< T as Ref[] > :: Item[l] == (& l T)))
+                )
+             )
+            ))
+
+   (traced '()
+           (test-term-false
+            (rust:can-prove-where-clause-in-program
+             Rust/Program
+             (∀ [(lifetime a) (type T)]
+                where [(for[(lifetime l)] (< T as Ref[] > :: Item[l] == (& l T)))]
+                ; key point here:
+                ;
+                ;     even though we know `for<'l> <T as Ref>::Item<'l> = &'l T`,
+                ;     we don't consider `<T as Ref>::Item<'static>` to be WF,
+                ;     because the implied where bound requires `T: 'static`.
+                ((< T as Ref[] > :: Item[static]) : OK[])
+                )
+             )
+            ))
+
+   (traced '()
+           (test-term-true
+            (rust:can-prove-where-clause-in-program
+             Rust/Program
+             (∀ [(lifetime a) (type T) (lifetime b)]
+                where [(for[(lifetime l)] (< T as Ref[] > :: Item[l] == (& l T)))
+                       (T : b)]
+                ; key point here:
+                ;^
+                ;     in contrast to previous test, we do consider `<T as Ref>::Item<'b>` OK,
+                ;     because we have the where-clause `T: 'b` in-scope.
+                ((< T as Ref[] > :: Item[b]) : OK[])
+                )
+             )
+            ))
    )
   )
+
