53a5a4906ca4431f8de60f4cc532ef774bfd5b82
Niko Matsakis <niko@alum.mit.edu>
2022-11-01 06:08:21 -0400

    tests

--- crates/formality-rust/src/lib.rs
+++ crates/formality-rust/src/lib.rs
@@ -1,4 +1,6 @@
-use formality_types::{derive_links, grammar::Fallible, parse::term};
+use formality_infer::Env;
+use formality_logic::{prove_universal_goal, UniversalGoalResult};
+use formality_types::{cast::Upcast, db::Db, derive_links, grammar::Fallible, parse::term};
 
 pub mod grammar;
 mod test;
@@ -13,4 +15,19 @@ pub fn check_program(program: &grammar::Program) -> Fallible<()> {
 
 pub fn test_program_ok(program_text: &str) -> Fallible<()> {
     formality_core::with_tracing_logs(|| check_program(&term(program_text)))
+}
+
+pub fn test_can_prove_where_clause(
+    program_text: &str,
+    where_clause: &str,
+) -> Fallible<UniversalGoalResult> {
+    formality_core::with_tracing_logs(|| -> Fallible<UniversalGoalResult> {
+        let rust_program: grammar::Program = term(program_text);
+        let decl_program = rust_program.to_decl()?;
+        let db = Db::new(decl_program);
+        let env = Env::default();
+        let goal: grammar::WhereClause = term(where_clause);
+        let predicate = goal.to_decl()?;
+        Ok(prove_universal_goal(&db, &env, &[], &predicate.upcast()))
+    })
 }
---
+++ crates/formality-rust/tests/basic-impl_Debug_for_i32.rs
@@ -0,0 +1,35 @@
+#![allow(non_snake_case)]
+
+const PROGRAM: &str = "[
+    crate core {
+        trait Debug<> where [] { }
+
+        impl<> Debug<> for i32 where [] { }
+    }
+]";
+
+#[test]
+fn test_i32() {
+    expect_test::expect![[r#"
+        Ok(
+            yes,
+        )
+    "#]]
+    .assert_debug_eq(&formality_rust::test_can_prove_where_clause(
+        PROGRAM,
+        "i32: Debug<>",
+    ));
+}
+
+#[test]
+fn test_u32() {
+    expect_test::expect![[r#"
+        Ok(
+            no,
+        )
+    "#]]
+    .assert_debug_eq(&formality_rust::test_can_prove_where_clause(
+        PROGRAM,
+        "u32: Debug<>",
+    ));
+}
