7db0e5800e2ae8e34770bf46d1501e39318dcaa8
Niko Matsakis <niko@alum.mit.edu>
2023-03-01 06:14:41 -0500

    kill unused imports

--- crates/formality-types/src/grammar/binder.rs
+++ crates/formality-types/src/grammar/binder.rs
@@ -14,7 +14,7 @@ use crate::{
 };
 
 use super::{
-    BoundVar, DebruijnIndex, Fallible, InferenceVar, Parameter, ParameterKind, PlaceholderVar,
+    BoundVar, DebruijnIndex, Fallible, Parameter, ParameterKind,
     Substitution, Variable,
 };
 
