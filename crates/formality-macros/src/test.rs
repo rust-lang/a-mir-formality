4471cf32daed39c6fce41e5af57f3acabcc57c8f
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-07-11 08:01:44 +0000

    Use `syn` convenience macro

--- crates/formality-macros/src/test.rs
+++ crates/formality-macros/src/test.rs
@@ -1,15 +1,11 @@
 use proc_macro::TokenStream;
-use quote::quote;
 
 pub(crate) fn test(_args: TokenStream, mut item_fn: syn::ItemFn) -> syn::Result<syn::ItemFn> {
     let original_fn_body = item_fn.block.clone();
-    item_fn.block = syn::parse2(quote! {
-        {
-            formality_core::with_tracing_logs(move || {
-                #original_fn_body
-            })
-        }
-    })
-    .unwrap();
+    item_fn.block = syn::parse_quote!({
+        formality_core::with_tracing_logs(move || {
+            #original_fn_body
+        })
+    });
     Ok(item_fn)
 }
