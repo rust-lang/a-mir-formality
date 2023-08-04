ba654a8624383faa5f84b1f66de71e8ea680fc46
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-08-04 11:36:41 +0000

    Don't panic on parser failure

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 use formality_check::check_all_crates;
 use formality_prove::{test_util::TestAssertion, Constraints};
 use formality_rust::grammar::Program;
-use formality_types::{collections::Set, parse::term};
+use formality_types::{collections::Set, parse::try_term};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -30,7 +30,7 @@ struct Args {
 pub fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let input: String = std::fs::read_to_string(&args.input_path)?;
-    let program: Program = term(&input);
+    let program: Program = try_term(&input)?;
 
     if args.print_rust {
         eprintln!("{:#?}", program);
@@ -40,14 +40,14 @@ pub fn main() -> anyhow::Result<()> {
 }
 
 pub fn test_program_ok(input: &str) -> anyhow::Result<()> {
-    let program: Program = term(&input);
+    let program: Program = try_term(&input)?;
     check_all_crates(&program)
 }
 
 pub fn test_where_clause(program: &str, assertion: &str) -> anyhow::Result<Set<Constraints>> {
-    let program: Program = term(&program);
+    let program: Program = try_term(&program)?;
     check_all_crates(&program)?;
-    let assertion: Arc<TestAssertion> = term(assertion);
+    let assertion: Arc<TestAssertion> = try_term(assertion)?;
     let decls = program.to_prove_decls();
     Ok(formality_prove::test_util::test_prove(decls, assertion))
 }
