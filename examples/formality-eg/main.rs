51ccd0cc582275be4dbe6c9e90458b3318627af3
Niko Matsakis <niko@alum.mit.edu>
2023-11-04 18:32:24 -0400

    start an example

---
+++ examples/formality-eg/main.rs
@@ -0,0 +1,26 @@
+mod grammar;
+mod type_system;
+
+formality_core::declare_language! {
+    mod eg {
+        const NAME = "Eg";
+        type Kind = crate::grammar::Kind;
+        type Parameter = crate::grammar::Parameter;
+        const BINDING_OPEN = '<';
+        const BINDING_CLOSE = '>';
+        const KEYWORDS = [
+            "struct",
+            "fn",
+            "let",
+            "in",
+        ];
+    }
+}
+
+// Default language for our crate
+use eg::FormalityLang;
+use formality_core::Fallible;
+
+fn main() -> Fallible<()> {
+    Ok(())
+}
