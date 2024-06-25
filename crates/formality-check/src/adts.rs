87873d957d19f004d020990d3e76df8598fa72b6
shua <gpg@isthisa.email>
2024-06-25 16:56:05 +0200

    check fields before trait solving

--- crates/formality-check/src/adts.rs
+++ crates/formality-check/src/adts.rs
@@ -9,6 +9,20 @@ impl super::Check<'_> {
     pub(super) fn check_adt(&self, adt: &Adt) -> Fallible<()> {
         let Adt { id: _, binder } = adt;
 
+        // names is used to check that there are no name conflicts
+        let mut names = HashSet::new();
+        for Variant { name, fields } in &adt.binder.open().1.variants {
+            if !names.insert((name, None)) {
+                bail!("variant \"{name:?}\" defined multiple times");
+            }
+            let vname = name;
+            for Field { name, ty: _ } in fields {
+                if !names.insert((vname, Some(name))) {
+                    bail!("field \"{name:?}\" of variant \"{vname:?}\" defined multiple times");
+                }
+            }
+        }
+
         let mut env = Env::default();
 
         let AdtBoundData {
@@ -18,17 +32,8 @@ impl super::Check<'_> {
 
         self.prove_where_clauses_well_formed(&env, &where_clauses, &where_clauses)?;
 
-        // names is used to check that there are no name conflicts
-        let mut names = HashSet::new();
-        for Variant { name, fields } in &variants {
-            if !names.insert((name, None)) {
-                bail!("variant \"{name:?}\" defined multiple times");
-            }
-            let vname = name;
-            for Field { name, ty } in fields {
-                if !names.insert((vname, Some(name))) {
-                    bail!("field \"{name:?}\" of variant \"{vname:?}\" defined multiple times");
-                }
+        for Variant { name: _, fields } in &variants {
+            for Field { name: _, ty } in fields {
                 self.prove_goal(&env, &where_clauses, ty.well_formed())?;
             }
         }
