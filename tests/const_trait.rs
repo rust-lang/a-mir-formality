84d8fb9640a61fc34dc5f5f854ca3b369b6a2eca
tiif <pekyuan@gmail.com>
2025-03-21 00:33:38 +0800

    Add test for const trait

--- tests/const_trait.rs
+++ tests/const_trait.rs
@@ -1,8 +1,11 @@
 #![allow(non_snake_case)] // we embed type names into the names for our test functions
 
-use a_mir_formality::test_program_ok;
+use a_mir_formality::{test_program_ok, test_where_clause};
+use expect_test::expect;
 use formality_core::test_util::ResultTestExt;
 use formality_macros::test;
+use formality_prove::{test_util::test_prove, Decls};
+use formality_types::rust::term;
 
 #[test]
 fn test_const_syntax() {
@@ -22,4 +25,20 @@ fn test_const_syntax() {
     };
 
     test_program_ok(&gen_program("")).assert_ok(expect_test::expect!["()"]);
+}
+
+// FIXME: this test is incorrect?
+#[test]
+fn test_const_trait_unprovable() {
+    let base: &str = "[
+        crate test {
+            const trait Foo {}
+        }
+    ]";
+
+    test_where_clause(
+        base,
+        "forall<ty T> { const Foo(T) } => { Foo(T) }",
+    )
+    .assert_ok(expect_test::expect!["{Constraints { env: Env { variables: [!ty_1], bias: Soundness }, known_true: true, substitution: {} }}"]);
 }
