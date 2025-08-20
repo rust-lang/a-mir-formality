f117fcfed08b83aeab68026212ae9ba10965fcd1
tiif <pekyuan@gmail.com>
2025-08-20 11:31:15 +0200

    add fixme

--- crates/formality-check/src/mini_rust_check.rs
+++ crates/formality-check/src/mini_rust_check.rs
@@ -278,6 +278,8 @@ impl Check<'_> {
             Field(field_projection) => {
                 let ty = self.check_place(env, &field_projection.root).unwrap();
 
+                // FIXME(tiif): We eventuallyw ant to do normalization here, so check_place should eventually be
+                // a judgment fn.
                 let Some(adt_id) = ty.get_adt_id() else {
                     bail!("The local used for field projection is not adt.")
                 };
