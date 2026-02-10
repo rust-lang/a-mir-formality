869f8408621d1a226b2bbf7e9feacfa1b0d0cd2b
Boxy Uwu <rust@boxyuwu.dev>
2026-02-10 15:37:30 +0000

    add exclamation mark

--- crates/formality-rust/src/prove/prove/prove/prove_wc.rs
+++ crates/formality-rust/src/prove/prove/prove/prove_wc.rs
@@ -1,5 +1,5 @@
-use formality_core::judgment_fn;
 use crate::types::grammar::{Predicate, Relation, Wc, WcData, Wcs};
+use formality_core::judgment_fn;
 
 use crate::prove::prove::{
     decls::Decls,
