09f95bf16b1f7815cb64ff44d256254f7dd3957f
Benno Lossin <benno.lossin@proton.me>
2026-06-02 14:49:59 +0200

    add test for field projections

---
+++ tests/field_projections.rs
@@ -0,0 +1,17 @@
+use a_mir_formality::{crates, FormalityTest};
+
+#[test]
+fn custom_ptr() {
+    FormalityTest::new(crates![crate test {
+        struct Ptr {}
+
+        impl Derefable for Ptr {
+            type Target = ();
+        }
+
+        fn test(ptr: Ptr) -> () {
+            let x: () = *ptr;
+        }
+    }])
+    .ok()
+}
